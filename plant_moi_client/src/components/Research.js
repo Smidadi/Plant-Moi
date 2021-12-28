fa5631e4619afe406a7ee7b0bb1216cb35ddb0ff
Pnixys <julien.daugaronthe@gmail.com>
2021-12-28 23:50:35 +0100

    Récupération de la valeur dans le champ recherche

--- plant_moi_client/src/components/Research.js
+++ plant_moi_client/src/components/Research.js
@@ -5,14 +5,24 @@ class Research extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { researchPlant: 'Rechercher une plante'};
+    this.state = { 
+      researchPlant: 'Rechercher une plante',
+      inputValue: ''
+    };
+  }
+
+  searchBarValue = (event) => {
+    this.setState({
+      inputValue: event.target.value
+    });
+    console.log(this.state.inputValue);
   }
 
   render() {
     return (
         <div class="research">
             <img src={logo} width="40px" height="40px"/> Plant&Moi
-            <input type="text" id="searchbar" name="search" placeholder={this.state.researchPlant}/>
+            <input type="text" id="searchbar" name="search" onChange={this.searchBarValue} value={this.state.inputValue} placeholder={this.state.researchPlant}/>
         </div>
     )
   }
