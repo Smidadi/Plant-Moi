9e40dfcdd95253a8edf6e90972beb3cd2562f181
Julien <50711686+Pnixys@users.noreply.github.com>
2022-01-14 22:30:13 +0100

    mise en place de la session utilisateur

--- plant_moi_client/src/components/Authentification.js
+++ plant_moi_client/src/components/Authentification.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import { ReactSession } from 'react-client-session';
 import connexionImg from '../img/connexion.png';
+
+import UserProfile from '../UserProfile';
 
 class Authentification extends Component { 
 
   render() {
     return (
         <div className="col-3 connexion"> 
-          <Link to="/Connexion">
+          <Link to={(ReactSession.get("connected") != true?"/Connexion":"/Profil")}>
             <img src={connexionImg} alt="Logo de connexion" width="90px" height="90px"/>
           </Link>
         </div>
