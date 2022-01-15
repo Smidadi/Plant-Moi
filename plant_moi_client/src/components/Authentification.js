4f71ae92881abd9919d188e912c017dc3b940898
Swwitcher <49500094+Swwitcher@users.noreply.github.com>
2022-01-15 21:17:44 +0100

    img deco (juste comme ça)

--- plant_moi_client/src/components/Authentification.js
+++ plant_moi_client/src/components/Authentification.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { ReactSession } from 'react-client-session';
 import connexionImg from '../img/connexion.png';
+import deco from '../img/se-deconnecter.png'
 
 import UserProfile from '../UserProfile';
+import Logo from './Logo';
 
 class Authentification extends Component { 
 
@@ -13,7 +15,9 @@ class Authentification extends Component {
           <Link to={(ReactSession.get("connected") != true?"/Connexion":"/Profil")}>
             <img src={connexionImg} alt="Logo de connexion" width="90px" height="90px"/>
           </Link>
+          <img src={deco} className="deco" width="50px"></img>
         </div>
+        
     )
   }
 }
