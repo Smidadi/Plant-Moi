31e7a335bcd58d8f02d6aa25465d7ad944e0f347
Smidadi <57463237+Smidadi@users.noreply.github.com>
2022-01-17 11:37:08 +0100

    totest

--- plant_moi_client/src/components/ListePlante.js
+++ plant_moi_client/src/components/ListePlante.js
@@ -7,22 +7,38 @@ class ListePlante extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        fav:''
+        plantName:'',
+        likedPlant:[]
     }
   }
 
-  test = () => {
+  componentDidMount = () => {
     fetch('http://localhost:5000/user/favPlant/'+ localStorage.getItem("username"))
     .then((res) => res.json())
-    .then((json) => console.log(json));
+    .then((json) => this.setState({
+        plantName: json.namePlant
+    }));
+
+    fetch('http://localhost:5000/user/likedPlant/'+ localStorage.getItem("username")).then((res) => res.json())
+    .then((json) => this.setState({
+        likedPlant: json.slice()
+    }));
+
+  }
+
+  test = () => {
+    let res;
+      for(let i = 0; i < this.state.likedPlant.length; ++i) {
+          res += <Plant plantName={this.state.likedPlant[i].namePlant}/>
+      }
+      return res;
   }
 
   render() {
     return (
         <>
-            <Plant plantName={this.test()}/>
-            <Plant />
-            <Plant />
+            <Plant plantName={this.state.plantName}/>
+            {test()}
         </>
     )
   }  
